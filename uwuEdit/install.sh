8a3b6d720cc047860a7cdf1f7bfcb14402e7ca64
Laar3 <laar@tutanota.com>
2022-03-06 11:14:38 +0000

    adding posix compliance

--- uwuEdit/install.sh
+++ uwuEdit/install.sh
@@ -2,7 +2,7 @@
 cp -r ./usr /
 cp ./uwuEdit /usr/bin/uwuedit
 
-if [[ -L "/bin" ]]; then
+if [ -L "/bin" ]; then
 	echo "/bin is a symlink, no need to copy..."
 else
 	echo "/bin isnt a symlink, symlinking (/bin/uwuEdit -> /usr/bin/uwuEdit)"
